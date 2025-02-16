2cb26ccb9df501b9d07f5892bd83bd3c50f730a2
Theviv1 <vivstock29@gmail.com>
2025-02-16 10:22:43 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-
+import './assets/main.css'
 import Vue3Toastify from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
 
