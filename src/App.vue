47806fad83510b3dd884244b924dfe0eac4fbcb9
Theviv1 <vivstock29@gmail.com>
2025-02-16 19:15:02 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="min-h-screen bg-[#111111] text-white">
+    <div class="">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from './stores/auth'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.initializeAuth()
+})
+</script>
+
+<style>
+@import 'bootstrap-icons/font/bootstrap-icons.css';
+
+#app {
+  font-family: 'DM Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+}
+
+.scrollbar-hide {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+</style>
